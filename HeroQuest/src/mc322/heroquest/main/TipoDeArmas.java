5077eaf2ad8b7e263b5d6a023ccb925933c46a11
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-24 20:00:01 -0300

    ll

--- HeroQuest/src/mc322/heroquest/main/TipoDeArmas.java
+++ HeroQuest/src/mc322/heroquest/main/TipoDeArmas.java
@@ -1,17 +1,20 @@
 package mc322.heroquest.main;
 
-/**
- * 
- */
 public enum TipoDeArmas {
-    ADAGA,
-    BASTAO,
-    ESPADA_LONGA,
-    ESPADA_CURTA,
-    ESPADA_LARGA,
-    MACHADO_DE_COMBATE,
-    BESTA,
-    PUNHAL,
-    ADAGA,
-    BASTAO
+    BASTAO(2),
+    ESPADA_LONGA(3),
+    ESPADA_CURTA(2),
+    MACHADO_DE_COMBATE(4),
+    LANCA(2),
+    PUNHAL(1);
+    
+    private int bonus;
+	
+	private TipoDeArmas(final int bonus) {
+		this.bonus = bonus;
+	}
+	
+	public int getBonus() {
+		return this.bonus;
+	}
 }
