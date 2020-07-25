11d3c2bd5fb9bc0d9aa6f729431be04ce244140e
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-25 15:42:25 -0300

    Fix

--- HeroQuest/src/mc322/heroquest/main/Heroi.java
+++ HeroQuest/src/mc322/heroquest/main/Heroi.java
@@ -104,9 +104,7 @@ public abstract class Heroi extends ElementoCombate{
     	this.bonusMovimento = bonus;
     }
       
-    protected void restauraVida(int vida) {
-    	
-    }
+    protected abstract void restauraVida(int vida);
 
     @Override
     public void defesaMagica(int ataque) {
