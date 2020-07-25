faff107546e43a884fee5afc8b0f7eb15eb82e82
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-25 14:59:49 -0300

    Update PocaoForca.java

--- HeroQuest/src/mc322/heroquest/main/PocaoForca.java
+++ HeroQuest/src/mc322/heroquest/main/PocaoForca.java
@@ -5,14 +5,12 @@ import java.util.*;
 public class PocaoForca extends Pocao {
 
     public PocaoForca() {
+    	super();
     	this.bonus = 2;
+    	this.nome += " de Forca";
     }
    
     protected void usar(Heroi heroi) {	
     	heroi.setBonusAtaque(bonus);
     }
-    public String getInformation(){
-        return "Pocao de Forca";
-    }
-
 }
