2d5e9f7e8442df7dadaf22a472b9bcdee553c3bc
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-25 20:17:46 -0300

    Novo

---
+++ HeroQuest/src/mc322/heroquest/main/ArmadilhaFlecha.java
@@ -0,0 +1,14 @@
+package mc322.heroquest.main;
+
+public class ArmadilhaFlecha extends Armadilha{
+	
+	ArmadilhaFlecha(Ponto posicao) {
+		super(posicao);
+		this.dano = 1;
+	}
+	
+	@Override
+	protected void ativar(Heroi heroi) {
+		heroi.perdeVida(dano);
+	}
+}
