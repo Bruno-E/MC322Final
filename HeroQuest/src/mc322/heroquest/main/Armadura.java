a94f87911dc30995ee6067f1e34fadf7af01600a
Bruno-E <68205423+Bruno-E@users.noreply.github.com>
2020-07-25 22:38:01 -0300

    Update Armadura.java

--- HeroQuest/src/mc322/heroquest/main/Armadura.java
+++ HeroQuest/src/mc322/heroquest/main/Armadura.java
@@ -3,15 +3,22 @@ package mc322.heroquest.main;
 import java.util.*;
 
 public class Armadura extends Item {
-    int bonus;
-    public Armadura() {
-    	this.nome = "Armadura";
-        this.bonus = 2;
-    }  
-	protected int getDados(Heroi heroi) {	
-	    return bonus;
-	}
-    public string getInformation(){
+
+
+    public Armadura(TipoDeArmaduras armadura) {
+    	this.armadura = armadura;
+    	this.nome = armadura.toString();
+    }
+
+    
+    private TipoDeArmaduras armadura;
+    
+    protected String getInformation(){
         return nome;
     }
+    
+    protected int getBonus() {
+    	return this.armadura.getBonus();
+    }
+
 }
