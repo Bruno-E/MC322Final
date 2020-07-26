23167259136f2dbc7138a4dd417ac825729120b3
garciinha <68205063+garciinha@users.noreply.github.com>
2020-07-26 02:58:05 +0000

    Update Feiticeiro.java

--- HeroQuest/src/mc322/heroquest/main/Feiticeiro.java
+++ HeroQuest/src/mc322/heroquest/main/Feiticeiro.java
@@ -1,7 +1,5 @@
 package mc322.heroquest.main;
 
-import java.util.*;
-
 public class Feiticeiro extends HeroiMagico {
     private static int VIDA_INICIAL = 4;
     private static int INT_INICIAL = 6;
